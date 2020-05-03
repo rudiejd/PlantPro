8db3005daca5554a16e7531a5f272c77653de8b2
JD Rudie <rudiejd@miamioh.edu>
2020-05-02 23:04:30 -0400

    update build script

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
-mysql --execute='CREATE DATABASE PlantPro';
-php artisan migrate; 
-php artisan test;
+sudo apt-get update -y && sudo apt-get upgrade -y; 
+
+if [-d "/var/www/html/PlantPro"];
+	then sudo rm -rf /var/www/html/PlantPro;
+fi
+
+git clone https://github.com/rudiejd/PlantPro.git /var/www/html;
+chmod 777 /var/www/html/PlantPro;
+
+echo 'PlantPro up to date! Enjoy!';
+
+
