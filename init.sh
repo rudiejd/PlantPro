578196c74924481d9c2029a939576e921fbb6ab2
JD Rudie <rudiejd@miamioh.edu>
2020-05-02 23:15:26 -0400

    fix build script again

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo apt-get update -y && sudo apt-get upgrade -y; 
 
-if [-d "/var/www/html/PlantPro"];
+if [ -d "/var/www/html/PlantPro" ];
 	then sudo rm -rf /var/www/html/PlantPro;
 fi
 
-git clone https://github.com/rudiejd/PlantPro.git /var/www/html;
-chmod 777 /var/www/html/PlantPro;
+git clone https://github.com/rudiejd/PlantPro.git /var/www/html/PlantPro;
+chmod 777 -R /var/www/html/PlantPro;
 
 echo 'PlantPro up to date! Enjoy!';
 
