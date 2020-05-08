526078cdbc1a0200a3821919b2222bcb56a45af2
rudiejd <rudiejd@users.noreply.github.com>
2020-05-08 17:48:51 -0400

    Update init.sh

--- build/init.sh
+++ build/init.sh
@@ -4,6 +4,7 @@ apt-get install git -y;
 apt install software-properties-common -y;
 add-apt-repository ppa:ondrej/ph -y; 
 apt install php7.4 php7.4-gd php7.4-mbstring php7.4-xml -y;
+apt install php-sqlite3 -y;
 apt install apache2 libapache2-mod-php7.4 -y;
 apt install mysql-server php7.4-mysql -y;
 curl -sS https://getcomposer.org/installer | php;
